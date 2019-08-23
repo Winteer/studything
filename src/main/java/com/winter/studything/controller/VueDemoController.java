35775641553fa89a42e616f1e5099b8d0382f3b2
Winnter <isnowash@yeah.net>
2019-08-23 17:01:28 +0800

    待添加-ttb后台。

--- src/main/java/com/winter/studything/controller/VueDemoController.java
+++ src/main/java/com/winter/studything/controller/VueDemoController.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:8080", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:20007", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class VueDemoController {

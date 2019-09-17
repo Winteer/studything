aa51c5c7f5125cdf97421d1c459450216723cd0b
Winnter <isnowash@yeah.net>
2019-09-17 15:10:05 +0800

    增加activiti工作流xml.

--- src/main/java/com/winter/studything/StudythingApplication.java
+++ src/main/java/com/winter/studything/StudythingApplication.java
@@ -1,10 +1,10 @@
 package com.winter.studything;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class StudythingApplication {
 
     public static void main(String[] args) {
